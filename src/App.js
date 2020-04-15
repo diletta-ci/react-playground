3d535da749a51048639e9477567f4438e04b76b9
Diletta Cala Impirotta <diletta.impirotta@huumun.com>
2020-04-15 17:13:56 +0200

    Counter - first game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
+import Counter from './Counter.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hello, World! </h1>
+
+        <div className="playground-container">
+            <div className="counter">
+                <Counter />
+            </div>
+        </div>
+
       </div>
     );
   }
