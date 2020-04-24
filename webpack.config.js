89685d24e28c45dcda2120f32e23b1979f09a2fd
Diletta Cala Impirotta <diletta.impirotta@huumun.com>
2020-04-24 17:37:47 +0200

    Build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,47 @@
 const path = require("path");
 const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: { presets: ["@babel/env"] }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
     },
-    resolve: {
-        extensions: ["*", ".js", ".jsx"],
-        alias: {
-           'react-dom': '@hot-loader/react-dom'
-        }
-    },
-    output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public/"),
-        port: 3000,
-        publicPath: "http://localhost:3000/dist/",
-        hotOnly: true,
-        compress: true,
-        disableHostCheck: true
-    },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true,
+    compress: true,
+    disableHostCheck: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyPlugin([
+      { from: 'public/', to: '' }
+    ])
+  ],
 };
